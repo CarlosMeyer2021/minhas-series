85d81b0021d6786ea82054348edf94bf0033153a
CarlosMeyer2021 <83259498+CarlosMeyer2021@users.noreply.github.com>
2022-02-02 21:05:23 -0300

    Series

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Header from "./Header"
+import {
+  BrowserRouter as Router,
+  Route
+}
+  from 'react-router-dom'
+
+const Home = () => {
+  return <h1>Home</h1>
+}
+
+const Generos = () => {
+  return <h1>Genêros</h1>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <Header />
+        <Route path='/' exact element={Home} />
+        <Route path="/generos" element={Generos} />
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Header.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import {
+  Navbar,
+  NavbarBrand,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarToggler
+} from 'reactstrap'
+
+const Header = () => {
+  const [open, setOpen] = useState(false)
+  const toggle = () => {
+    setOpen(!open)
+  }
+  return (
+    <Navbar color="light" light expand='md'>
+      <NavbarBrand>Minhas Séries</NavbarBrand>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={open} navbar>
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink href="/">Genêros</NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </Navbar>
+  )
+}
+
+export default Header
